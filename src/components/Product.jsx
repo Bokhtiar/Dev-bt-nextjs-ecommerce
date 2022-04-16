14eecee816bafcf2f4edb33d7fd4abeec890ece0
bokhtiar <bokhtiartoshar1@gmail.com>
2022-04-16 16:10:10 +0600

    product list page show

---
+++ src/components/Product.jsx
@@ -0,0 +1,39 @@
+const Product = ({products}) => {
+    return (
+        <div>
+           
+                <div className="container">
+                <div className="row">
+                {
+                    products.map((product, index)=>
+                        <div className="col-12 col-md-3 col-lg-3 my-3">
+                            <div class="card" style={{ width: "16rem", height:"460px" }}>
+                                <img src={product.image} class="card-img-top" style={{ height:"300px" }} alt="..."/>
+                                <div class="card-body">
+                                    <h5 class="card-title">{product.title}</h5>
+                                    <div className="form-inline text-center">
+                                       <p className="text-center"> 
+                                            <a href="#" class="btn btn-primary">a</a>&nbsp;
+                                            <a href="#" class="btn btn-primary">a</a>&nbsp;
+                                            <a href="#" class="btn btn-primary">a</a>&nbsp;
+                                        </p>
+                                    </div>
+                                
+                                </div>
+                            </div>
+                        </div>
+                        
+                    )
+                }   
+                </div>
+                    </div>
+            
+            <p>asdfsadfsd</p>
+        </div>
+    )
+}
+
+
+
+export default Product
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,22 @@
 import Head from "next/head"
 import Layout from "../components/Layout"
+import Product from "../components/Product"
 import Slider from "../components/Slider"
 
-export default function Home() {
+export default function Home({products}) {
   return (
 	<div>
       <Layout>
        <Slider />
+       <Product products={products}/>
       </Layout>
 	</div>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch(`https://fakestoreapi.com/products`)
+  const products = await response.json()
+  console.log(products)
+  return { props: { products } }
 }
