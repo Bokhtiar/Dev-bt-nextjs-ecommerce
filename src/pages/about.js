499c86ac08444a02cb76a593ae3ccaf9727f7e6a
bokhtiar <bokhtiartoshar1@gmail.com>
2022-04-16 14:34:53 +0600

    templete masting navbar footer layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
-export default function about() {
+export default function about({data}) {
+
+  
+
+
   return (
     <div>
-      <Layout>about us</Layout>
+      <Layout>about us
+      {
+        data.map((dt, index)=>
+          <li>{dt.title}</li>
+        )
+      }
+      </Layout>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const res1 = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const data = await res1.json()
+  console.log(data)
+  return { 
+      props: { 
+          data,
+      } 
+  }
+
 }
