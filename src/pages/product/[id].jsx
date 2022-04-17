c5e4b5be76f0c2dfc3eb62073e5e27091ffd9324
bokhtiar <bokhtiartoshar1@gmail.com>
2022-04-17 11:32:22 +0600

    product desing page

---
+++ src/pages/product/[id].jsx
@@ -0,0 +1,18 @@
+import Layout from "../../components/Layout"
+
+const ProductDetail = () => {
+    return (
+        <Layout>
+            <section className="container">
+                <div className="row">
+                    <div className="col-md-4 col-lg-4 col-sm-12">asdfsad</div>
+                    <div className="col-md-8 col-lg-8 col-sm-12">adsfasdf</div>
+                </div>
+            </section>
+        </Layout>
+    )
+}
+
+
+
+export default ProductDetail
