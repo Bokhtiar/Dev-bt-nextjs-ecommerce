d721863b3cfe71d9d62ce916c002dabc586a0d96
bokhtiar <bokhtiartoshar1@gmail.com>
2022-04-17 12:31:43 +0600

    some update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-            <Link class="navbar-brand" href={'/'}>Navbar</Link>
+            <a class="navbar-brand " href={'/'}> <img src="/imgs/f.png" height={'50px'} alt="" /> </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -13,7 +13,10 @@ const Navbar = () => {
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href={'/about'}>About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href={'/about'}>About</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href={'/product'}>Product</a>
---
+++ src/pages/about.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function about({data}) {
+
+  
+
+
+  return (
+    <div>
+      <Layout>
+        <section className='container'>
+          about us page
+        </section>
+      </Layout>
+    </div>
+  )
+}
